c22af3ea7f7f4e74ac07fe2d97a593f4bb637830
usman676asif <87007295+usman676asif@users.noreply.github.com>
2022-01-15 00:25:17 +0500

    Login screen added and define theme colors

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,25 @@ import React from 'react';
 import './App.css';
 import Home from './screens/Home';
 import Login from './screens/Login';
+import { createTheme, ThemeProvider } from '@mui/material';
 
+const theme = createTheme({
+  palette: {
+    colors : {
+      primary: '#4f1150',
+      secondary: '#e69acb',
+      white:'#ffffff',
+    },
+  },
+});
 function App() {
   return (
-    <div>
-      <Home/>
-      {/* <Login/> */}
-    </div>
+    <ThemeProvider theme={theme}>
+      <div>
+        {/* <Home/> */}
+        <Login/>
+      </div>
+    </ThemeProvider>
   );
 }
 
--- src/screens/Login.jsx
+++ src/screens/Login.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { LoginWrapper } from '../styles/Login';
 import TextField from '@mui/material/TextField';
-import { Button, colors, InputAdornment, OutlinedInput } from '@mui/material';
+import { Button, colors, IconButton, InputAdornment, OutlinedInput, Stack, Box, FormControl, InputLabel } from '@mui/material';
 import AccountCircle from '@mui/icons-material/AccountCircle';
 import LockIcon from '@mui/icons-material/Lock';
 import BackgroundImage from '../img/loginBackgroundImage.jpg'; 
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
 
 
 const Login = () => {
@@ -12,6 +14,12 @@ const Login = () => {
     const [values, setValues] = React.useState({
         username: '',
         password: '',
+        showPassword :false,
+        invalidPassword: false,
+        invalidUsername: false,
+        usernameError: '',
+        passwordError: '',
+
     });
 
     const handleChange = (prop) => (event) => {
@@ -24,62 +32,101 @@ const Login = () => {
             showPassword: !values.showPassword,
         });
     };
+
+    const handleMouseDownPassword = (event) => {
+        event.preventDefault();
+      };
     
     const handleSubmit = () =>{
+        setValues({
+            ...values,
+            invalidUsername : false,
+            usernameError: '',
+            invalidPassword : false,
+            passwordError:'',
+        });
+      
+        if(values.username.length == 0){
+            setValues({
+                ...values,
+                invalidUsername : true,
+                usernameError: 'Username cannot be empty',
+            });
+        } else if(values.password.length == 0){
+            setValues({
+                ...values,
+                invalidPassword : true,
+                passwordError: 'password cannot be empty',
+            });
+        }
 
+        // submit login
     }
+
+
     return (
         <LoginWrapper style={{ 
             backgroundImage: `url(${BackgroundImage})`,
              backgroundSize: "cover",
             height: "100vh",
         }}>
-            <div className="login-card" >
+            <Box 
+                sx={{
+                    width:'400px',
+                    height: '80%',
+                    bgcolor: 'colors.white',
+                    color: 'secondary.contrastText',
+                    p: 2,
+                }}
+                className="login-card" >
                 <form>
                     <div className="heading-div d-flex flex-direction-column justify-content-center algin-items-center">
                         <LockIcon className='lock-icon'/>
-                        <h2>Singn In</h2>
+                        <h2>Sign In</h2>
                     </div>
                     <TextField 
                         className="input"
+                        error={values.invalidUsername}
+                        helperText={values.usernameError}
                         id="outlined-basic" 
                         label="Username" 
                         variant="outlined"
+                        value={values.username}
+                        onChange={handleChange('username')}
                         startAdornment={
                             <InputAdornment position="start">
-                              <AccountCircle />
+                            <AccountCircle />
                             </InputAdornment>
-                          } />
-                <OutlinedInput
-                    id="outlined-adornment-password"
-                    type={values.showPassword ? 'text' : 'password'}
-                    value={values.password}
-                    onChange={handleChange('password')}
-                    endAdornment={
-                    <InputAdornment position="end">
-                        <IconButton
-                        aria-label="toggle password visibility"
-                        onClick={handleClickShowPassword}
-                        onMouseDown={handleMouseDownPassword}
-                        edge="end"
-                        >
-                        {values.showPassword ? <VisibilityOff /> : <Visibility />}
-                        </IconButton>
-                    </InputAdornment>
-                    }
-                    label="Password"
-                />
-                    <TextField 
-                        className="input"
-
-                        id="outlined-basic" 
-                        label="Password" 
-                        variant="outlined" />
-                    <div className="login-div d-flex justify-content-center">
-                        <Button onClick={handleSubmit} className="login-btn"  color='success' variant="contained">Sign In</Button>
+                        } />
+                    <FormControl className='input' variant="outlined">
+                        <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
+                        <OutlinedInput
+                            id="outlined-adornment-password"
+                            error={values.invalidPassword}
+                            helperText={values.passwordError}
+                            type={values.showPassword ? 'text' : 'password'}
+                            value={values.password}
+                            onChange={handleChange('password')}
+                            endAdornment={
+                            <InputAdornment position="end">
+                                <IconButton
+                                aria-label="toggle password visibility"
+                                onClick={handleClickShowPassword}
+                                onMouseDown={handleMouseDownPassword}
+                                edge="end"
+                                >
+                                {values.showPassword ? <VisibilityOff /> : <Visibility />}
+                                </IconButton>
+                            </InputAdornment>
+                            }
+                            label="Password"
+                        />
+                    </FormControl>
+                    <div className="btn-div">
+                        <Button onClick={handleSubmit} className="login-btn"  variant="contained">Sign In</Button>
                     </div>
                 </form>
-            </div>
+            </Box>
         </LoginWrapper>
     );
 };
